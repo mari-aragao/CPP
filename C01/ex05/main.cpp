f9341ff4fc60d0012b52b9ec2e670b830ce19e0a
maragao <mariazevedoaragao@gmail.com>
2023-07-18 21:26:22 -0300

    add C01/ex05

---
+++ C01/ex05/main.cpp
@@ -0,0 +1,15 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+    Harl    harl;
+
+    harl.complain("DEBUG");
+    std::cout << std::endl;
+    harl.complain("INFO");
+    std::cout << std::endl;
+    harl.complain("WARNING");
+    std::cout << std::endl;
+    harl.complain("ERROR");
+    return 0;
+}
