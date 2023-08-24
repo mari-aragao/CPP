2f9bf1f78b02f40d349bfdbac25cdc2a4c379ec1
mari-aragao <mariazevedoaragao@gmail.com>
2023-08-24 14:28:54 -0300

    add more tests on C07/ex00

--- C07/ex00/main.cpp
+++ C07/ex00/main.cpp
@@ -24,25 +24,33 @@ int main(void)
     char e = 'e';
     char f = 'f';
 
+    std::string g = "chaine1";
+    std::string h = "chaine2";
+
     std::cout << "*** Comparison between int, floats and char ***" << std::endl << std::endl;
     std::cout << "Original Values: int a = " << a << " | int b = " << b << std::endl;
-    std::cout << "Max value: " << max<int>(a, b) << std::endl;
-    std::cout << "Min value: " << min<int>(a, b) << std::endl;
-    swap<int>(a, b);
+    std::cout << "Min value: " << ::min(a, b) << std::endl;
+    std::cout << "Max value: " << ::max(a, b) << std::endl;
+    ::swap(a, b);
     std::cout << "After swap: int a = " << a << " | int b = " << b << std::endl << std::endl;
     
     std::cout << "Original Values: float c = " << c << " | float d = " << d << std::endl;
-    std::cout << "Max value: " << max<float>(c, d) << std::endl;
-    std::cout << "Min value: " << min<float>(c, d) << std::endl;
-    swap<float>(c, d);
+    std::cout << "Min value: " << ::min(c, d) << std::endl;
+    std::cout << "Max value: " << ::max(c, d) << std::endl;
+    ::swap(c, d);
     std::cout << "After swap: float c = " << c << " | float d = " << d << std::endl << std::endl;
     
     std::cout << "Original Values: char e = " << e << " | char f = " << f << std::endl;
-    std::cout << "Max value: " << max<char>(e, f) << std::endl;
-    std::cout << "Min value: " << min<char>(e, f) << std::endl;
-    swap<char>(e, f);
+    std::cout << "Min value: " << ::min(e, f) << std::endl;
+    std::cout << "Max value: " << ::max(e, f) << std::endl;
+    ::swap(e, f);
     std::cout << "After swap: char e = " << e << " | char f = " << f << std::endl << std::endl;
 
+    std::cout << "Original Values: std::string g = " << g << " | std::string h = " << h << std::endl;
+    std::cout << "Min value: " << ::min(g, h) << std::endl;
+    std::cout << "Max value: " << ::max(g, h) << std::endl;
+    ::swap(g, h);
+    std::cout << "After swap: std::string g = " << g << " | std::string h = " << h << std::endl << std::endl;
 
     std::cout << "*** Comparison between classes of int, floats and char ***" << std::endl << std::endl;
     Whatever<int> wa(a);
@@ -51,24 +59,32 @@ int main(void)
     Whatever<float> wd(d);
     Whatever<char> we(e);
     Whatever<char> wf(f);
+    Whatever<std::string> wg(g);
+    Whatever<std::string> wh(h);
 
     std::cout << "Original Values: class of int wa = " << wa.getValue() << " | class of int wb = " << wb.getValue() << std::endl;
-    std::cout << "Max value: " << (max< Whatever<int> >(wa, wb)).getValue() << std::endl;
-    std::cout << "Min value: " << (min< Whatever<int> >(wa, wb)).getValue() << std::endl;
-    swap<Whatever<int> >(wa, wb);
+    std::cout << "Min value: " << (::min<Whatever <int> >(wa, wb)).getValue() << std::endl;
+    std::cout << "Max value: " << (::max<Whatever <int> >(wa, wb)).getValue() << std::endl;
+    ::swap<Whatever <int> >(wa, wb);
     std::cout << "After swap: class of int wa = " << wa.getValue() << " | class of int wb = " << wb.getValue() << std::endl << std::endl;
     
     std::cout << "Original Values: class of float wc = " << wc.getValue() << " | class of float wd = " << wd.getValue() << std::endl;
-    std::cout << "Max value: " << (max< Whatever<float> >(wc, wd)).getValue() << std::endl;
-    std::cout << "Min value: " << (min< Whatever<float> >(wc, wd)).getValue() << std::endl;
-    swap<Whatever<float> >(wc, wd);
+    std::cout << "Min value: " << (::min<Whatever <float> >(wc, wd)).getValue() << std::endl;
+    std::cout << "Max value: " << (::max<Whatever <float> >(wc, wd)).getValue() << std::endl;
+    ::swap<Whatever <float> >(wc, wd);
     std::cout << "After swap: class of float wc = " << wc.getValue() << " | class of float wd = " << wd.getValue() << std::endl << std::endl;
     
     std::cout << "Original Values: class of char we = " << we.getValue() << " | class of char wf = " << wf.getValue() << std::endl;
-    std::cout << "Max value: " << (max< Whatever<char> >(we, wf)).getValue() << std::endl;
-    std::cout << "Min value: " << (min< Whatever<char> >(we, wf)).getValue() << std::endl;
-    swap<Whatever<char> >(we, wf);
+    std::cout << "Min value: " << (::min<Whatever <char> >(we, wf)).getValue() << std::endl;
+    std::cout << "Max value: " << (::max<Whatever <char> >(we, wf)).getValue() << std::endl;
+    ::swap<Whatever <char> >(we, wf);
     std::cout << "After swap: class of char we = " << we.getValue() << " | class of char wf = " << wf.getValue() << std::endl << std::endl;
+
+    std::cout << "Original Values: class of std::string wg = " << wg.getValue() << " | class of std::string wh = " << wh.getValue() << std::endl;
+    std::cout << "Min value: " << (::min<Whatever <std::string> >(wg, wh)).getValue() << std::endl;
+    std::cout << "Max value: " << (::max<Whatever <std::string> >(wg, wh)).getValue() << std::endl;
+    ::swap<Whatever< std::string> >(wg, wh);
+    std::cout << "After swap: class of std::string wg = " << wg.getValue() << " | class of std::string wh = " << wh.getValue() << std::endl << std::endl;
 
     return 0;
 }
